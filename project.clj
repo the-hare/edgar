25dcb828068d98fa325f239612800b5fd829cb5a
Timothy Washington <twashing@gmail.com>
2013-02-03 19:49:43 -0500

    -- some work adding jtsclient and referencing it in the local repo

--- project.clj
+++ project.clj
@@ -2,8 +2,10 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.5"]]
+                 [compojure "1.1.5"]
+                 [jtsclient/jtsclient "9.8.3"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler edgar.handler/app}
-  :profiles
-  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+  :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}}
+  :repositories {"local" ~(str (.toURI (java.io.File. "/Users/timothyw/.m2/repository/")))}
+  )
