fb442e1acdae28e8cac1f8e00cc4fd9f8bb5bbf2
Timothy Washington <twashing@gmail.com>
2013-04-12 14:09:08 -0400

    -- fixing bug so I can create NYSE contracts and submit to IB's SMART router

--- src/edgar/ib/market.clj
+++ src/edgar/ib/market.clj
@@ -11,5 +11,12 @@
   (socket/connect-to-tws))
 
 (defn request-market-data [client idx instrm]
-  (.reqMktData client idx (Contract. idx, instrm, "STK", nil, 0.0, nil, nil, "SMART", "USD", nil, nil, nil, false, nil, nil) nil false)
+
+  (let [contract (Contract.)]
+    (set! (.m_symbol contract) instrm)
+    (set! (.m_secType contract) "STK")
+    (set! (.m_exchange contract) "SMART")
+    (set! (.m_currency contract) "USD")
+
+    (.reqMktData client idx contract "" true))
   )
