50b5aab2d68a970351ec4b0bce0584be2f90b5cc
Timothy Washington <twashing@gmail.com>
2013-04-18 11:20:39 -0400

    -- removing code cruft and peeling event-handler into a separate function

--- src/edgar/ib/loader.clj
+++ src/edgar/ib/loader.clj
@@ -11,28 +11,6 @@
             )
   )
 
-#_(defn get-stock-lists [client]
-
-  (with-open [amexfile (io/reader "etc/amexlist.csv")
-              nysefile (io/reader "etc/nyselist.csv")
-              nasdaqfile (io/reader "etc/nasdaqlist.csv")
-              ]
-
-    (let [amexlist   (csv/read-csv amexfile)
-          nyselist   (csv/read-csv nysefile)
-          nasdaqlist (csv/read-csv nasdaqfile)]
-
-      #_(reduce (fn [rslt ech]
-
-                (println (<< "calling reqMktData on [~{(-> ech first string/trim)}]"))
-                (market/request-market-data client rslt (-> ech first string/trim))
-                (inc rslt))
-              0
-              (doall (take 100 (rest nyselist))))
-      )
-    )
-  )
-
 (defn get-stock-lists []
 
   (let [amexfile (io/reader "etc/amexlist.csv")
@@ -46,103 +24,108 @@
     ))
 
 
+#_(defn- next-bucket-id []
+    ;; ... TODO
+    )
+
+;; subscribe to EWrapper mkt data events
+(defn- snapshot-handler [bucket-hundred rst]
+
+
+  ;; (splitter/pushEvent rst)
+
+  (let [
+        ;;bucket-hundred (ref [])
+        event-index (first (first
+                           (filter (fn [inp] (= (:id (second inp))
+                                               (rst "tickerId") ))
+                                   (map-indexed vector @bucket-hundred) )))
+        ]
+
+    (println "snapshot-handler > event index [" event-index "] > result [" rst "] > bucket-hundred [" @bucket-hundred "]")
+
+
+    ;; ***
+    ;; bucket-hundred will have a structure of: [ { :id rslt :symbol stock-sym :event-list [] } ]
+    ;; i) go into the bucket, ii) find the appropriate element and iii) insert event into the :event-list
+    (dosync (alter bucket-hundred
+                   (fn [blist]
+                     (update-in blist
+                                [ event-index :event-list ]
+                                (fn [inp]
+                                  (conj inp rst))))))
+
+
+    ;; ***
+    ;; insert price difference, iff type is "historicalData"
+    (if (and (= "historicalData" (rst "type"))
+             (-> (rst "high") nil? not)
+             (-> (rst "low") nil? not))
+
+      ;; find this iteration's price difference
+      (let [price-difference (- (rst "high")
+                                (rst "low"))
+            prev-difference (:price-difference (nth @bucket-hundred event-index))]
+
+        ;; determine if greater than the existing price-difference
+        (if (and (-> prev-difference nil? not)
+                 (> price-difference prev-difference))
+
+          (dosync (alter bucket-hundred
+                         update-in
+                         [ event-index ]
+                         (fn [inp]
+                           (merge inp { :price-difference price-difference })) )))))
+
+
+    ;; ***
+    ;; Order list by largest price difference
+    (dosync (alter bucket-hundred
+                   (fn [inp]
+                     (into []   ;; put the result into a vector
+                           (sort-by :price-difference > inp)))
+                   ))
+
+
+
+    (pprint/pprint @bucket-hundred)
+
+    ;; when getting stock data, when results arrive, decide if
+    ;;
+    ;; i. it's within the top 100 price ranges
+    ;; ii. if not, discard,
+
+    ;; ii.i get the next ID
+
+    ;; ii.ii) get the next stock
+
+    ;; ii.iii) reqMarketData for that next stock
+
+
+    ;; ... TODO
+    ;; check if bucket-hundred has reached the 100 threshold
+    ;; if yes... go through and find daily high & lows; calculate the difference
+    ;; keep the largest 100 differences, and discard the rest
+
+
+    )
+
+  )
+
 (defn filter-price-movement
   "Run through stocks and filter based on the stocks that have the biggest high / low price movement"
   [client]
 
 
   ;; get first 100 stocks
-  (let [stock-lists (get-stock-lists)
-        first-hundred (take 10 (rest (:nyselist stock-lists)))
+  (let [bucket-hundred (ref [])
+        stock-lists (get-stock-lists)
+        first-hundred (take 1 (rest (:nyselist stock-lists)))
         after-hundred (nthrest (rest (:nyselist stock-lists)) 101)
-
-        bucket-hundred (ref [])
         ]
 
 
-    #_(defn- next-bucket-id []
-      ;; ... TODO
-      )
-
-    ;; subscribe to EWrapper mkt data events
-    (defn- snapshot-handler [rst]
-
-
-      ;; (splitter/pushEvent rst)
-
-      (let [event-index (first (first
-                               (filter (fn [inp] (= (:id (second inp))
-                                                   (rst "tickerId") ))
-                                       (map-indexed vector @bucket-hundred) )))
-            ]
-
-        (println "snapshot-handler > event index [" event-index "] > result [" rst "] > bucket-hundred [" @bucket-hundred "]")
-
-
-        ;; ***
-        ;; bucket-hundred will have a structure of: [ { :id rslt :symbol stock-sym :event-list [] } ]
-        ;; i) go into the bucket, ii) find the appropriate element and iii) insert event into the :event-list
-        (dosync (alter bucket-hundred
-                       (fn [blist]
-                         (update-in blist
-                                    [ event-index :event-list ]
-                                    (fn [inp]
-                                      (conj inp rst))))))
-
-
-        ;; ***
-        ;; insert price difference, iff type is "historicalData"
-        (if (and (= "historicalData" (rst "type"))
-                 (-> (rst "high") nil? not)
-                 (-> (rst "low") nil? not))
-
-          ;; find this iteration's price difference
-          (let [price-difference (- (rst "high")
-                                    (rst "low"))
-                prev-difference (:price-difference (nth @bucket-hundred event-index))]
-
-            ;; determine if greater than the existing price-difference
-            (if (and (-> prev-difference nil? not)
-                     (> price-difference prev-difference))
-
-              (dosync (alter bucket-hundred
-                             update-in
-                             [ event-index ]
-                             (fn [inp]
-                               (merge inp { :price-difference price-difference })) ))))))
-
-
-      ;; ***
-      ;; Order list by largest price difference
-      (dosync (alter bucket-hundred
-                     (fn [inp]
-                       (into []   ;; put the result into a vector
-                             (sort-by :price-difference > inp)))
-                     ))
-
-      (pprint/pprint @bucket-hundred)
-
-      ;; when getting stock data, when results arrive, decide if
-      ;;
-      ;; i. it's within the top 100 price ranges
-      ;; ii. if not, discard,
-
-      ;; ii.i get the next ID
-
-      ;; ii.ii) get the next stock
-
-      ;; ii.iii) reqMarketData for that next stock
-
-
-      ;; ... TODO
-      ;; check if bucket-hundred has reached the 100 threshold
-      ;; if yes... go through and find daily high & lows; calculate the difference
-      ;; keep the largest 100 differences, and discard the rest
-
-
-      )
-
-    (market/subscribe-to-market snapshot-handler)
+    (market/subscribe-to-market (partial snapshot-handler bucket-hundred))
 
     ;; reqMarketData for first 100 stocks
     (reduce (fn [rslt ech]
