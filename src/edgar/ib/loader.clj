763b61c3a2b01a3e19528add304384031e9dc4f7
Timothy Washington <twashing@gmail.com>
2013-04-26 15:00:47 -0400

    -- adding some more comments before handing off to datomic persist code -- adding historical data schema

--- src/edgar/ib/loader.clj
+++ src/edgar/ib/loader.clj
@@ -178,12 +178,28 @@
       ;; ii.iii) reqMarketData for that next stock; repeat constantly through: NYSE, NASDAQ, AMEX
       (dosync (alter bucket (fn [inp]
 
-                              (log/debug "... in remove > inp[" inp "]")
                               (into []
                                     (remove #(= rid (:id %)) inp)
                                     ))))
       (market/cancel-market-data client rid)
 
+      ;; ... TODO: push to Tee / Datomic; Data structure looks like:
+      [{:id 0,
+        :symbol DDD,
+        :company 3D Systems Corporation,
+        :price-difference 1.3100000000000023,
+        :event-list [{high 35.11,
+                      tickerId 0,
+                      WAP 34.491,
+                      open 35.07,
+                      date 20130426,
+                      count 3403,
+                      low 33.8,
+                      hasGaps false,
+                      close 34.53,
+                      field historicalData,
+                      volume 8667,
+                      type historicalData}]}]
       ))
     )
 
