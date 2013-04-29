4bb39b713cc8a3be582361163289debb50c98c35
Timothy Washington <twashing@gmail.com>
2013-04-29 11:46:55 -0400

    -- read in historical records; i) order by price-difference (get the top one) ; ii) limit to 100 -- listen for, then subscribe to market-data

--- src/edgar/core/edgar.clj
+++ src/edgar/core/edgar.clj
@@ -2,28 +2,43 @@
   (:use [clojure.repl]
         [clojure.core.strint]
         [datomic.api :only [q db] :as d])
-  (:require [edgar.datomic :as edatomic])
+  (:require [edgar.datomic :as edatomic]
+            [edgar.ib.market :as market])
   )
 
 
-(defn asdf []
+(defn load-historical-data []
 
   ;; find entity.symbol (and entire entity)
   ;; where price-difference is greatest
+  (let [historical-entities (q '[:find ?p ?s ?c :where
+                                 [?h :historical/price-difference ?p]
+                                 [?h :historical/symbol ?s]
+                                 [?h :historical/company ?c]
+                                 ] (db edatomic/conn))]
+    (sort-by first historical-entities)
+    )
 
-  (def historical-ids (q '[:find ?p ?s ?c :where
+
+  #_(def historical-ids (q '[:find ?p ?s ?c :where
                            [?h :historical/price-difference ?p]
                            [?h :historical/symbol ?s]
                            [?h :historical/company ?c]
                            ] (db edatomic/conn)))
 
-  (def the-list (into [] historical-ids))
-  (def thing (sort-by first the-list))
-  (def allthings (doall (map println thing)))
-
+  ;;(def the-list (into [] historical-ids))
+  ;;(def thing (sort-by first the-list))
+  ;;(def allthings (doall (map println thing)))
 
   ;;(def historical-entities (map (fn [[hid]] (d/entity (db edatomic/conn) hid)) historical-ids))
   ;;(def historical-sorted (sort-by :historical/price-difference historical-entities))
 
-
   )
+
+(defn test-run []
+
+  (let [client (:esocket (market/connect-to-market))
+        hdata (load-historical-data client)]
+
+    (market/request-market-data client 0 (-> hdata last second) false)
+    ))
