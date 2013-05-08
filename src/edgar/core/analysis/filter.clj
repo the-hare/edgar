8525e3501604640d71db7baf35afa939c3caaa16
Timothy Washington <twashing@gmail.com>
2013-05-08 09:43:11 -0400

    -- setting up skeleton of exponential-moving-average function -- mod'ing the simple-moving-average to include the :last-trade-price

--- src/edgar/core/analysis/filter.clj
+++ src/edgar/core/analysis/filter.clj
@@ -19,15 +19,39 @@
     (reduce (fn [rslt ech]
 
               (let [tsum (reduce (fn [rslt inp]
-
                                    (let [ltprice (:last-trade-price inp)]
                                      (+ (if (string? ltprice) (read-string ltprice) ltprice) rslt))) 0 ech)   ;; sum it up
                     taverage (/ tsum (count ech))   ;; get the average
                     ]
 
-                (cons {:last-trade-price-average taverage :last-trade-time (:last-trade-time (first ech))} rslt)) )
+                (cons {:last-trade-price (:last-trade-price (first ech))
+                       :last-trade-price-average taverage
+                       :last-trade-time (:last-trade-time (first ech))} rslt) ))
 
             ma-list  ;; begin with a reversed tick-list, over which we can iterate
             (reverse (partition tick-window 1 tick-list)))
     )
 )
+
+(defn exponential-moving-average
+  "From a tick-list, generates an accompanying exponential moving average list.
+
+     EMA = price(today) * k + EMA(yesterday) * (1 - k)
+     k = 2 / N + 1
+     N = number of days
+
+   Returns a list, equal in length to the tick-list, but only with slots filled,
+   where preceding tick-list allows."
+
+  ([tick-window tick-list]
+
+     (exponential-moving-average tick-window tick-list (simple-moving-average tick-window tick-list)))
+
+  ([tick-window tick-list sma-list]
+
+     ;; EMA = price(today) * k + EMA(yesterday) * (1 - k)
+     ;; k = 2 / N + 1
+     ;; N = number of days
+
+     )
+  )
