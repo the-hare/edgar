dcbe14f37458ca84e085c985d71aa13152c629df
Timothy Washington <twashing@gmail.com>
2013-05-08 10:36:03 -0400

    -- finished first pass of exponential-moving-average function

--- src/edgar/core/analysis/filter.clj
+++ src/edgar/core/analysis/filter.clj
@@ -11,8 +11,7 @@
         start-index tick-window
 
         ;; back fill slots with nils, into an accompanying moving-average list
-        ma-list (into '() (repeat tick-window nil))
-        ]
+        ma-list (into '() (repeat tick-window nil))]
 
 
     ;; calculate Simple Moving Average for each slot there's a window
@@ -26,12 +25,12 @@
 
                 (cons {:last-trade-price (:last-trade-price (first ech))
                        :last-trade-price-average taverage
-                       :last-trade-time (:last-trade-time (first ech))} rslt) ))
+                       :last-trade-time (:last-trade-time (first ech))} rslt)))
 
             ma-list  ;; begin with a reversed tick-list, over which we can iterate
             (reverse (partition tick-window 1 tick-list)))
     )
-)
+  )
 
 (defn exponential-moving-average
   "From a tick-list, generates an accompanying exponential moving average list.
@@ -49,9 +48,31 @@
 
   ([tick-window tick-list sma-list]
 
-     ;; EMA = price(today) * k + EMA(yesterday) * (1 - k)
+     ;; 1. calculate 'k'
      ;; k = 2 / N + 1
      ;; N = number of days
+     (let [k (/ 2 (+ tick-window 1))
+           ema-list (into '() (repeat tick-window nil))]
 
+
+       ;; 2. get the simple-moving-average for a given tick - 1
+       (reduce (fn [rslt inp]
+
+                 ;; 3. calculate the EMA ( for the first tick, EMA(yesterday) = MA(yesterday) )
+
+                 (let [;; price(today)
+                       ltprice (:last-trade-price inp)
+
+                       ;; EMA(yesterday)
+                       ema-last (:last-trade-price-exponential (first rslt))
+
+                       ;; ** EMA now = price(today) * k + EMA(yesterday) * (1 - k)
+                       ema-now (+ (* k ltprice) (* ema-last (- 1 k)))]
+
+                   (cons {:last-trade-price (first ech)
+                          :last-trade-time (first ech)
+                          :last-trade-price-exponential ema-now} rslt)))
+               ema-list
+               (->> sma-list (remove nil?) (partition 2 1) reverse)))
      )
   )
