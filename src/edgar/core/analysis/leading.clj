eed6ca254f33a002937af0a79cadf3c9e98a31ca
Timothy Washington <twashing@gmail.com>
2013-05-13 20:39:41 -0400

    -- successful first pass at %K in the stochastic ocsillator

--- src/edgar/core/analysis/leading.clj
+++ src/edgar/core/analysis/leading.clj
@@ -68,19 +68,86 @@
        ;; compute the difference, or divergence
        (let [macd-list (into '() (repeat signal-window nil))]
 
-         (map (fn [e-macd e-ema]
+         (cons
+          (map (fn [e-macd e-ema]
 
-                (if (and (-> e-macd nil? not)
-                         (-> e-ema nil? not))
+                 (if (and (-> e-macd nil? not)
+                          (-> e-ema nil? not))
 
-                  {:last-trade-price (:last-trade-price e-macd)
-                   :last-trade-time (:last-trade-time e-macd)
-                   :histogram (- (:last-trade-macd e-macd) (:ema-signal e-ema))}
-                  ))
-              macd
-              ema-signal
-              )
+                   {:last-trade-price (:last-trade-price e-macd)
+                    :last-trade-time (:last-trade-time e-macd)
+                    :histogram (- (:last-trade-macd e-macd) (:ema-signal e-ema))}
+                   ))
+               macd
+               ema-signal
+               )
+          macd-list)
          )
        )
      )
+)
+
+(defn stochastic-oscillator
+  "The stochastic oscillator is a momentum indicator. According to George C. Lane (the inventor), it 'doesn't follow price, it doesn't follow volume or anything like that. It follows the speed or the momentum of price. As a rule, the momentum changes direction before price'. A 3-line Stochastics will give an anticipatory signal in %K, a signal in the turnaround of %D at or before a bottom, and a confirmation of the turnaround in %D-Slow. Smoothing the indicator over 3 periods is standard. Returns a list, equal in length to the tick-list, but only with slots filled, where preceding tick-list allows.
+
+     i) last-price:
+       the last closing price
+
+     ii) %K:
+       (last-price - low-price / high-price - low-price) * 100
+
+     iii) %D:
+       3-period exponential moving average of %K
+
+     iv)  %D-Slow
+       a 3-period exponential moving average of %D
+
+     v) low-price:
+       the lowest price over the last N periods
+
+     vi) high-price:
+       the highest price over the last N periods
+
+
+   The inputs to this function are:
+
+     tick-window: the length of Stochastic, or number of ticks under observation (defaults to 14)
+     trigger-window: the smoothing line (defaults to 3)
+     trigger-line: (defaults to 3)
+     tick-list: the input time series (in last trade price)"
+
+  [tick-window trigger-window trigger-line tick-list]
+
+
+  (let [stochastic-list (into '() (repeat tick-window nil))]
+
+
+    (reduce (fn [rslt ech]
+
+              (let [last-price (:last-trade-price (first ech))
+
+                    last-price-list (map #(if (string? (:last-trade-price %))
+                                            (read-string (:last-trade-price %))
+                                            (:last-trade-price %)) ech)
+
+                    highest-price (apply max last-price-list)
+                    lowest-price (apply min last-price-list)
+
+                    ;; calculate %K
+                    %K (/ (- last-price lowest-price) (- highest-price lowest-price))
+
+
+                    ;;xxx (println "... last-price-list[" last-price-list "] / high[" highest-price "] low[" lowest-price "] PRICE[" last-price "] ...k[" %K "]")
+
+                    ;; calculate %D
+
+                    ]
+
+                (cons {:last-price last-price
+                       :highest-price highest-price
+                       :lowest-price lowest-price
+                       :K %K} rslt)))
+            stochastic-list
+            (partition tick-window 1 tick-list))
+    )
 )
