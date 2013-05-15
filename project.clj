196b2d58aaa7d49c692d1e8de6065f30a0075770
Timothy Washington <twashing@gmail.com>
2013-05-15 11:32:44 -0400

    -- adding pedestal files for migrating edgar to that framework -- migrating project.clj to include pedestal libraries

--- project.clj
+++ project.clj
@@ -1,10 +1,22 @@
 (defproject edgar "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Edgar is a basic tool for performing technical Analysis on financial instruments"
   :url "http://example.com/FIXME"
-  :source-paths ["src"]
+  ;;:source-paths ["src"]
   :java-source-paths ["src"]
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.5"]
+  :dependencies [
+                 ;; Pedestal libraries
+                 [org.clojure/clojure "1.5.0"]
+                 [io.pedestal/pedestal.service "0.1.6"]
+
+                 [io.pedestal/pedestal.jetty "0.1.6"]
+
+                 ;; Logging
+                 [ch.qos.logback/logback-classic "1.0.7"]
+                 [org.slf4j/jul-to-slf4j "1.7.2"]
+                 [org.slf4j/jcl-over-slf4j "1.7.2"]
+                 [org.slf4j/log4j-over-slf4j "1.7.2"]
+                 
+                 ;; Edgar proper
                  [org.clojure/core.incubator "0.1.2"]
                  [jtsclient/jtsclient "9.8.3"]
                  [org.clojure/data.csv "0.1.2"]
@@ -14,22 +26,18 @@
                  [lamina "0.5.0-beta15"]
                  [aleph "0.3.0-beta15"]
                  [clj-time "0.5.0"]
-                 [org/clojure/data.json "0.1.0"]
                  [com.datomic/datomic "0.8.3335"
                   :exclusions [org.slf4j/slf4j-nop org.slf4j/log4j-over-slf4j]]
-                 [org.slf4j/slf4j-log4j12 "1.6.4"]
-                 [ring/ring-jetty-adapter "1.2.0-SNAPSHOT"]
-
-
+                 
                  ;; Clojurescript Libraries
                  [shoreleave/shoreleave-remote "0.3.0"]
                  [shoreleave/shoreleave-remote-ring "0.3.0"]
                  [jayq "2.3.0"]
 
-
                  ;; Java Libraries
                  [joda-time "2.2"]
                  ]
+                
   :plugins [[lein-ring "0.8.2"]
             [lein-cljsbuild "0.3.0"]]
   :cljsbuild {
@@ -44,9 +52,12 @@
                                    :optimizations :simple ;; :whitespace ;; :advanced ;; :simple
                                    :pretty-print true}}]}
   :ring {:handler edgar.handler/app}
-  :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}}
+  :profiles {:dev {:source-paths ["src", "dev"] 
+                   :dependencies [[ring-mock "0.1.3"]]}}
   :resource-paths ["etc/:public/"]
   :repositories { "local" ~(str (.toURI (java.io.File. (str (-> (load-file "etc/config.clj") :dev :root-dir) ".m2/repository/"))))
                   }
-  :main edgar.handler
+  ;;:main edgar.handler
+  :min-lein-version "2.0.0"
+  :main ^{:skip-aot true} edgar.server
   )
