8aa6c145444f55055c033b0d428f230aa8b5ef53
Timothy Washington <twashing@gmail.com>
2013-05-17 21:45:50 -0400

    -- for tee/play, removing the tick structure transform -- verifying that the event time data is getting properly captured

--- src/edgar/tee/play.clj
+++ src/edgar/tee/play.clj
@@ -11,46 +11,21 @@
 
 
 (defn tee-market
- "Process the list of entities. First, flatten out the :event-list, and merge it into the entity
-
- [{:id 0,
-   :symbol DDD,
-   :company 3D Systems Corporation,
-   :price-difference 1.3100000000000023,
-   :event-list [{high 35.11,
-                 tickerId 0,
-                 WAP 34.491,
-                 open 35.07,
-                 date 20130426,
-                 count 3403,
-                 low 33.8,
-                 hasGaps false,
-                 close 34.53,
-                 field historicalData,
-                 volume 8667,
-                 type historicalData}]}]"
+ "Process the list of entities. First, flatten out the :event-list, and merge it into the entity"
  [tick-list tick]
 
 
- ;; transform tick structure
- (let [final-tick {:historical/symbol (:symbol tick)
-                   :historical/company (:company tick)
-                   :historical/price-difference (:price-difference tick)
+ (println "tee.play/tee-market > tick[" tick "] / tick-list[" (count tick-list) "][" tick-list "]")
 
-                   :historical/high (-> tick :event-list first (get "high" 0.0))
-                   :historical/low (-> tick :event-list first (get "low" 0.0))
-                   :historical/WAP (-> tick :event-list first (get "WAP" 0.0))
 
-                   :historical/open (-> tick :event-list first (get "open" 0.0))
-                   :historical/close (-> tick :event-list first (get "close" 0.0))
+ ;; make sure we are recieving time data
 
-                   :historical/date (-> tick :event-list first (get "date" ""))
-                   :historical/count (-> tick :event-list first (get "count" 0))
-                   :historical/hasGaps (-> tick :event-list first (get "hasGaps" false))
-                   :historical/volume (-> tick :event-list first (get "volume" 0))
-                   }
-       ]
+ ;; do historical play
 
-   (println "tee.play/tee-market > final-tick[" final-tick "] / tick-list[" (count tick-list) "][" tick-list "]")
+ ;; create signals for each filter
 
-   ))
+ ;; create strategy, which invokes a signal, based on:
+ ;;   the price against a filter
+ ;;   wrap a publish / subscribe model around signals
+
+ )
