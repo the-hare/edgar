b7dfb519a6317d053c192dcf335a6661d96e56f9
Timothy Washington <twashing@gmail.com>
2013-05-29 12:44:55 -0400

    -- abstracting functionality out to join-averages

--- src/edgar/core/signal/lagging.clj
+++ src/edgar/core/signal/lagging.clj
@@ -2,6 +2,40 @@
   (:require [edgar.core.analysis.lagging :as analysis]))
 
 
+(defn join-averages
+  "Create a list where i) tick-list ii) sma-list and iii) ema-list are overlaid"
+
+  ([tick-window tick-list]
+
+     (let [sma-list (analysis/simple-moving-average nil tick-window tick-list)
+           ema-list (analysis/exponential-moving-average nil tick-window tick-list sma-list)]
+       (moving-averages tick-window tick-list sma-list ema-list)))
+
+  ([tick-window tick-list sma-list ema-list]
+     (map (fn [titem sitem eitem]
+
+
+            #_(println (str "... titem[" (:last-trade-time titem) "] sitem[" (:last-trade-time sitem) "] eitem[" (:last-trade-time eitem)
+                            "] > MATCH[" (= (:last-trade-time titem) (:last-trade-time sitem) (:last-trade-time eitem)) "]"))
+
+            ;; 1. ensure that we have the :last-trade-time for simple and exponential items
+            ;; 2. ensure that all 3 time items line up
+            (if (and (and (not (nil? (:last-trade-time sitem)))
+                          (not (nil? (:last-trade-time eitem))))
+                     (= (:last-trade-time titem) (:last-trade-time sitem) (:last-trade-time eitem)))
+
+              {:last-trade-time (:last-trade-time titem)
+               :last-trade-price (:last-trade-price titem)
+               :last-trade-price-average (:last-trade-price-average sitem)
+               :last-trade-price-exponential (:last-trade-price-exponential eitem)}
+
+              nil))
+
+          tick-list
+          sma-list
+          ema-list)))
+
+
 (defn moving-averages
   "Takes baseline time series, along with 2 other moving averages.
 
@@ -17,32 +51,8 @@
   ([tick-window tick-list sma-list ema-list]
 
      ;; create a list where i) tick-list ii) sma-list and iii) ema-list are overlaid
-
-     (let [joined-list (map (fn [titem sitem eitem]
-
-
-                              #_(println (str "... titem[" (:last-trade-time titem) "] sitem[" (:last-trade-time sitem) "] eitem[" (:last-trade-time eitem)
-                                               "] > MATCH[" (= (:last-trade-time titem) (:last-trade-time sitem) (:last-trade-time eitem)) "]"))
-
-                              ;; 1. ensure that we have the :last-trade-time for simple and exponential items
-                              ;; 2. ensure that all 3 time items line up
-                              (if (and (and (not (nil? (:last-trade-time sitem)))
-                                            (not (nil? (:last-trade-time eitem))))
-                                       (= (:last-trade-time titem) (:last-trade-time sitem) (:last-trade-time eitem)))
+     (let [joined-list (join-averages tick-window tick-list sma-list ema-list)]
 
 
-                                {:last-trade-time (:last-trade-time titem)
-                                 :last-trade-price (:last-trade-price titem)
-                                 :last-trade-price-average (:last-trade-price-average sitem)
-                                 :last-trade-price-exponential (:last-trade-price-exponential eitem)}
-
-                                nil))
-
-                            tick-list
-                            sma-list
-                            ema-list)]
-
-       joined-list)
-     ;; find time points where ema-list (or second list) crosses over the sma-list (or 1st list)
-
-     ))
+       ;; find time points where ema-list (or second list) crosses over the sma-list (or 1st list)
+       joined-list)))
