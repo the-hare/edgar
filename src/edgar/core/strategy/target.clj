0056ef04ba4e1e15fdd5351910a00a53209b72a3
Timothy Washington <twashing@gmail.com>
2013-06-17 20:32:31 -0400

    -- added rudimentary target functions to the strategy ns

--- src/edgar/core/strategy/target.clj
+++ src/edgar/core/strategy/target.clj
@@ -8,13 +8,15 @@
 
     change-pct))
 
+
+
+;; Making a default stop / loss threshold of -2.5%
 (defn stoploss-threshhold? [orig-price current-price]
 
   (let [change-pct (percentage-change orig-price current-price)
         change-test (> -0.025 change-pct)]
 
     change-test))
-
 
 (defn stoploss-threshhold-wstocks? [orig-price current-price no-shares]
 
@@ -23,5 +25,24 @@
         balance-trg (+ balance-org (* balance-org -0.025))
         change-test (< balance-crt balance-trg)]
 
-    (println (str "... > test[" change-test "] > current[" balance-crt "] > threshold[" balance-trg "]"))
+    change-test))
+
+
+
+
+;; Making a default target threshold of 5%
+(defn target-threshhold? [orig-price current-price]
+
+  (let [change-pct (percentage-change orig-price current-price)
+        change-test (>= change-pct 0.05)]
+
+    change-test))
+
+(defn target-threshhold-wstocks? [orig-price current-price no-shares]
+
+  (let [balance-org (* orig-price no-shares)
+        balance-crt (* current-price no-shares)
+        balance-trg (* balance-org 1.05)
+        change-test (>= balance-crt balance-trg)]
+
     change-test))
